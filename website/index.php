9c7c2f0d5841d0f18c335e439ba5d891fe89802e
Tiago José Valdrich <tiago.valdrich@hotmail.com>
2018-08-10 22:30:49 -0300

    Using the Slim Framework for creating routes.

--- website/index.php
+++ website/index.php
@@ -1,5 +1,17 @@
 <?php
 
-    phpinfo();
+    require_once("vendor/autoload.php");
+
+    $app = new \Slim\Slim();
+
+    $app->get('/', function () {
+        echo "HOMEPAGE!!";
+    });
+
+    $app->get('/hello/:name', function ($name) {
+        echo "Hello, " . $name;
+    });
+
+    $app->run();
 
 ?>
